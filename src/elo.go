04550129a19e700d6f66e3084445b4ce73d645ec
Roland Lamidieu <roland.lamidieu@gmail.com>
2022-01-23 16:54:28 +0100

    Create compute ratings functions

---
+++ src/elo.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"math"
+)
+
+var D float64 = 400.0
+var K float64 = 32.0
+
+/// Compute updated ratings for all players
+func ComputeRatings(ratings []float64, positions []int) []float64 {
+	updatedRatings := make([]float64, len(ratings))
+	for i := range ratings {
+		expected := computeExpectedScore(i, ratings)
+		actual := computeActualScore(positions[i], len(ratings))
+		updatedRating := computeUpdatedRating(ratings[i], expected, actual, len(ratings))
+		updatedRatings[i] = updatedRating
+	}
+	return updatedRatings
+}
+
+/// Returns the updated score for a player, given his expected and actual score
+func computeUpdatedRating(rating, expectedScore, actualScore float64, N int) float64 {
+	return rating + K*float64(N-1.0)*(actualScore-expectedScore)
+}
+
+/// Returns the actual score, given the player's position
+/// 1 if the player finished 1st and 0 if he finished last
+func computeActualScore(position, N int) float64 {
+	return float64(N-position) / (float64(N*(N-1)) / 2.0)
+}
+
+/// Returns the expected score for a player, given all ratings
+/// The expected score is between 0 and 1 and represents a probability of winning
+func computeExpectedScore(currentIndex int, ratings []float64) float64 {
+	N := len(ratings)
+	sum := 0.0
+	for i := range ratings {
+		if i != currentIndex {
+			sum += expectedTwoPlayersScore(ratings[currentIndex], ratings[i])
+		}
+	}
+	return sum / (float64(N*(N-1)) / 2.0)
+}
+
+/// Returns the expected score for player A, given playerA and playerB's current ratings
+func expectedTwoPlayersScore(ratingA, ratingB float64) float64 {
+	return 1.0 / (1.0 + math.Pow(10.0, (ratingB-ratingA)/D))
+}
--- src/server.go
+++ src/server.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 )
@@ -18,9 +20,86 @@ func headers(w http.ResponseWriter, req *http.Request) {
 	}
 }
 
+type createPlayerRequest struct {
+	Name string `json:"name"`
+}
+
+func createPlayer(w http.ResponseWriter, req *http.Request) {
+	b, err := io.ReadAll(req.Body)
+	if err != nil {
+		handleError(w, err)
+		return
+	}
+	var body createPlayerRequest
+	jsonErr := json.Unmarshal(b, &body)
+	if jsonErr != nil {
+		handleError(w, jsonErr)
+		return
+	}
+	fmt.Fprintf(w, "Creating \"%v\"...\n", body.Name)
+	// TODO Create player
+}
+
+type submitResultsRequest struct {
+	Results []string `json:"results"`
+}
+
+type submitResultsResponse struct {
+	Players []player `json:"players"`
+}
+
+type player struct {
+	Id     string  `json:"id"`
+	Name   string  `json:"name"`
+	Rating float64 `json:"rating"`
+}
+
+func handleError(w http.ResponseWriter, err error) {
+	fmt.Printf("Error: %v\n", err)
+	fmt.Fprintf(w, "Error: %v", err)
+}
+
+func submitResults(w http.ResponseWriter, req *http.Request) {
+	b, err := io.ReadAll(req.Body)
+	if err != nil {
+		handleError(w, err)
+		return
+	}
+	var body submitResultsRequest
+	jsonErr := json.Unmarshal(b, &body)
+	if jsonErr != nil {
+		handleError(w, jsonErr)
+		return
+	}
+
+	fmt.Printf("Submitting results %v...\n", body.Results)
+	submitResultsResponse := submitResultsResponse{
+		Players: []player{
+			{
+				Id:     "one",
+				Name:   "One",
+				Rating: 1000.0,
+			},
+			{
+				Id:     "two",
+				Name:   "Two",
+				Rating: 1002.0,
+			},
+		},
+	}
+	responseBytes, resErr := json.Marshal(&submitResultsResponse)
+	if resErr != nil {
+		handleError(w, resErr)
+		return
+	}
+	fmt.Fprint(w, string(responseBytes))
+}
+
 func main() {
 	http.HandleFunc("/hello", hello)
 	http.HandleFunc("/headers", headers)
+	http.HandleFunc("/createPlayer", createPlayer)
+	http.HandleFunc("/submitResults", submitResults)
 
 	httpPort := os.Getenv("HTTP_PORT")
 	if httpPort == "" {
