7575cc8414c2abf39ea4506183261f43730a7af0
Roland Lamidieu <roland.lamidieu@gmail.com>
2022-06-13 07:21:38 +0200

    feat: Add see Season 1 button

--- server/web/ranking.go
+++ server/web/ranking.go
@@ -3,6 +3,7 @@ package web
 import (
 	"fmt"
 	"net/http"
+	"strconv"
 
 	"github.com/thalkz/kart/config"
 	"github.com/thalkz/kart/database"
@@ -17,18 +18,29 @@ type RankingPage struct {
 	UnrankedPlayers    []models.Player
 }
 
+func parseSeason(r *http.Request) int {
+	seasonStr := r.FormValue("season")
+	season, err := strconv.Atoi(seasonStr)
+	if seasonStr == "" || err != nil {
+		return config.Season
+	}
+	return season
+}
+
 func RankingHandler(w http.ResponseWriter, r *http.Request) error {
 	if r.URL.Path != "/" {
 		http.NotFound(w, r)
 		return nil
 	}
 
-	rankedPlayers, err := database.GetRankedPlayers(config.Season, config.MinRacesCount)
+	season := parseSeason(r)
+
+	rankedPlayers, err := database.GetRankedPlayers(season, config.MinRacesCount)
 	if err != nil {
 		return fmt.Errorf("failed to get ranked players: %w", err)
 	}
 
-	unrankedPlayers, err := database.GetUnrankedPlayers(config.Season, config.MinRacesCount)
+	unrankedPlayers, err := database.GetUnrankedPlayers(season, config.MinRacesCount)
 	if err != nil {
 		return fmt.Errorf("failed to get unranked players: %w", err)
 	}
@@ -39,7 +51,7 @@ func RankingHandler(w http.ResponseWriter, r *http.Request) error {
 	}
 
 	data := RankingPage{
-		Season:             config.Season,
+		Season:             season,
 		TimeUntilSeasonEnd: timeUntil,
 		MinRacesCount:      config.MinRacesCount,
 		RankedPlayers:      rankedPlayers,
